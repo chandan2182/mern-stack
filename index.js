6ffd9c06cec2e38e858b4627b3c29e2327237ba1
chandan2182 <139637586+chandan2182@users.noreply.github.com>
2023-07-26 20:05:25 +0530

    arrays.js

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log('hello java script!!');
+
+console.log('another line');
+
+age=27;
+
+console.log(age);
+
+console.log(typeof age);
+
+message='nice';
+
+console.log(message);
+
+ishungrey= true;
+
+console.log(typeof ishungrey);
+
+if(age>=18){
+    console.log('you are eligible for DL');
+    x='this should be global.';
+    let y='this should be local';
+    var z='this should be local';
+    const pi=3.14;
+    //pi= 24.158476
+}else
+{
+    console.log('you are not eligible for DL');
+}
+console.log(x);
+
+//console.log (y);
+
+console.log(z);
+
+console.log(pi);
+
+// airthmatics operatiors
+// +: addition
+// -: substraction
+// *: multipilication
+// /: divide
+
+
+
+
